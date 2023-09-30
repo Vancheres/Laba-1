80626c4c192be2121c63ce7cdd8813c2e138f3b0
Vancheres <anisii840@gmail.com>
2023-09-30 16:40:39 +0500

    Результаты Лаб. работы №1

---
+++ main.py
@@ -0,0 +1,14 @@
+a = float(input("Введите первое число (a): "))
+b = float(input("Введите второе число (b): "))
+
+sum_result = a + b
+subtract_result = a - b
+multiply_result = a * b
+average_result = (a + b) / 2
+difference_abs_result = max(abs(a), abs(b)) - min(abs(a), abs(b))
+
+print(f"Сумма чисел {a} и {b} = {sum_result}")
+print(f"Разность чисел {a} и {b} = {subtract_result}")
+print(f"Произведение чисел {a} и {b} = {multiply_result}")
+print(f"Среднее арифметическое чисел {a} и {b} = {average_result}")
+print(f"Разность максимального и минимального по модулю = {difference_abs_result}")
